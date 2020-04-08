49b8059974064a013e82744699c1aee6f7bd1649
paprota55 <44095450+paprota55@users.noreply.github.com>
2020-04-08 22:59:02 +0200

    Added arm moving and Sprite locked
    Arm rotating dependent on mouse position.
    Lock sprite in character, not all of player component change position with player move.

---
+++ Shooter-Game/Assets/Standard Assets/2D/Scripts/ArmRotating.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmRotating : MonoBehaviour
+{
+    public int offset = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
+        Vector3 lookPos = Camera.main.ScreenToWorldPoint(mousePos);
+        lookPos = lookPos - transform.position;
+        float angle = Mathf.Atan2(lookPos.y, lookPos.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, angle + offset);
+;
+    }
+}
--- Shooter-Game/Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
+++ Shooter-Game/Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
@@ -20,6 +20,8 @@ namespace UnityStandardAssets._2D
         private Rigidbody2D m_Rigidbody2D;
         private bool m_FacingRight = true;  // For determining which way the player is currently facing.
 
+        Transform characterSprite;          // Reference to sprite which is included to character
+
         private void Awake()
         {
             // Setting up references.
@@ -27,6 +29,13 @@ namespace UnityStandardAssets._2D
             m_CeilingCheck = transform.Find("CeilingCheck");
             m_Anim = GetComponent<Animator>();
             m_Rigidbody2D = GetComponent<Rigidbody2D>();
+
+            characterSprite = transform.FindChild("Character"); // 
+
+            if(characterSprite == null)
+            {
+                Debug.LogError("I cannot load character sprite");
+            }
         }
 
 
@@ -106,9 +115,9 @@ namespace UnityStandardAssets._2D
             m_FacingRight = !m_FacingRight;
 
             // Multiply the player's x local scale by -1.
-            Vector3 theScale = transform.localScale;
+            Vector3 theScale = characterSprite.localScale;
             theScale.x *= -1;
-            transform.localScale = theScale;
+            characterSprite.localScale = theScale;
         }
     }
 }
