63d6c30deeb8588f148c0ad2b26449eba72c481a
paprota55 <44095450+paprota55@users.noreply.github.com>
2020-04-10 15:42:01 +0200

    Added shooting to game
    Created shooting and bullet script. Created bullet prefab.

---
+++ Shooter-Game/Assets/Scripts/Shooting.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Transform startPosition;
+    public GameObject bulletPrefab;
+
+    public float speed = 20f;
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, startPosition.position, startPosition.rotation);
+        Rigidbody2D rigidBody = bullet.GetComponent<Rigidbody2D>();
+        rigidBody.AddForce(startPosition.right * speed, ForceMode2D.Impulse);
+    }
+}
