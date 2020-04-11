85b9206257a2fd658f279485a9eb71a680d2f207
paprota55 <44095450+paprota55@users.noreply.github.com>
2020-04-11 10:46:14 +0200

    Added player destroying when die

---
+++ Shooter-Game/Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerStats
+{
+    public float Health = 100;
+
+}
+
+public class Player : MonoBehaviour
+{
+    public PlayerStats playerStats = new PlayerStats();
+
+    public int atitudeLimitToDie = -10;
+    void Update()
+    {
+        if(transform.position.y <= -20)
+        {
+            attackPlayer(playerStats.Health);
+        }
+    }
+
+    public void attackPlayer(float damage)
+    {
+        playerStats.Health -= damage;
+
+        if(playerStats.Health <=0)
+        {
+            GameController.kill(this);
+        }
+    }
+}
--- Shooter-Game/Assets/Standard Assets/2D/Scripts/Camera2DFollow.cs
+++ Shooter-Game/Assets/Standard Assets/2D/Scripts/Camera2DFollow.cs
@@ -10,6 +10,8 @@ namespace UnityStandardAssets._2D
         public float lookAheadFactor = 3;
         public float lookAheadReturnSpeed = 0.5f;
         public float lookAheadMoveThreshold = 0.1f;
+        public float downLimit = 0;
+        public float upLimit = 0;
 
         private float m_OffsetZ;
         private Vector3 m_LastTargetPosition;
@@ -28,6 +30,7 @@ namespace UnityStandardAssets._2D
         // Update is called once per frame
         private void Update()
         {
+            if (target == null) return;
             // only update lookahead pos if accelerating or changed direction
             float xMoveDelta = (target.position - m_LastTargetPosition).x;
 
@@ -44,6 +47,8 @@ namespace UnityStandardAssets._2D
 
             Vector3 aheadTargetPos = target.position + m_LookAheadPos + Vector3.forward*m_OffsetZ;
             Vector3 newPos = Vector3.SmoothDamp(transform.position, aheadTargetPos, ref m_CurrentVelocity, damping);
+
+            newPos = new Vector3(newPos.x,Mathf.Clamp(newPos.y,downLimit,upLimit), newPos.z);
 
             transform.position = newPos;
 
