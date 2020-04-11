429c602a603c260d6459e08a07362d15ed571c73
paprota55 <44095450+paprota55@users.noreply.github.com>
2020-04-11 11:17:21 +0200

    Added player respawn

--- Shooter-Game/Assets/Scripts/GameController.cs
+++ Shooter-Game/Assets/Scripts/GameController.cs
@@ -4,10 +4,31 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    public static GameController gm;
+
+
+    private void Start()
+    {
+        if(gm == null)
+        {;
+            gm = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+        }
+    }
+
     public static void kill(Player player)
     {
         Destroy(player.gameObject);
+        gm.respawnPlayer();
     }
 
+    public Transform spawnPoint;
+    public Transform playerObject;
+
+
+    public void respawnPlayer()
+    {
+        Instantiate(playerObject, spawnPoint.position, spawnPoint.rotation);
+        Debug.Log("Spawn player");
+    }
 
 }
