79eebd34454bfee471dc5b9f5ce25d2d233c8326
paprota55 <44095450+paprota55@users.noreply.github.com>
2020-04-14 16:50:51 +0200

    Added bullet destroy effect

--- Shooter-Game/Assets/Scripts/Bullet.cs
+++ Shooter-Game/Assets/Scripts/Bullet.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public GameObject effect;
     public float damage = 50;
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -16,10 +17,18 @@ public class Bullet : MonoBehaviour
                 enemy.DamageEnemy(damage);
             }
             Destroy(gameObject);
+            Effect();
         }
         else
         {
             Destroy(gameObject);
+            Effect();
         }
+    }
+
+    void Effect()
+    {
+        GameObject effectPrefab = Instantiate(effect, gameObject.transform.position, gameObject.transform.rotation);
+        Destroy(effectPrefab, 0.5f);
     }
 }
