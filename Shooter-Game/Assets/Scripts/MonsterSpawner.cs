b859c918b5808c7b3c43cd2be249d6655ec51a3c
paprota55 <44095450+paprota55@users.noreply.github.com>
2020-04-20 23:17:13 +0200

    Added monster spawner script. Updated status UI.

---
+++ Shooter-Game/Assets/Scripts/MonsterSpawner.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Wave
+{
+    public GameObject enemy;
+    public int quantity;
+    public float delay;
+    public int whichWave;
+}
+
+public enum State { SPAWN, WAIT, COUNT };
+
+public class MonsterSpawner : MonoBehaviour
+{
+    private State state = State.COUNT;
+    public Wave[] wavesList;
+    public GameObject[] spawnPoints;
+    private int whichWave = 0;
+    public float timeToNextWave = 8f;
+    private float counterToNextWave;
+
+    private void Start()
+    {
+        counterToNextWave = timeToNextWave;
+    }
+
+    private void Update()
+    {
+        if(state == State.WAIT)
+        {
+            if (IsEnemyAlive() == false)
+            {
+
+                arrangementsToNextWave();
+                return;
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        if(counterToNextWave <= 0)
+        {
+            if(state == State.COUNT)
+            {
+                StartCoroutine(Spawn(wavesList[whichWave]));
+            }
+        }
+        else
+        {
+            counterToNextWave -= Time.deltaTime;
+        }
+    }
+
+    void arrangementsToNextWave()
+    {
+        state = State.COUNT;
+        counterToNextWave = timeToNextWave;
+        whichWave += 1;
+        if (whichWave  > wavesList.Length - 1)
+        {
+            whichWave = 0;
+        }
+    }
+
+    bool IsEnemyAlive()
+    {
+        if(GameObject.FindGameObjectsWithTag("Enemy").Length == 0)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    IEnumerator Spawn(Wave wave)
+    {
+        state = State.SPAWN;
+
+        for(int i = 0;i<wave.quantity;i++)
+        {
+            SpawnEnemy(wave.enemy);
+            yield return new WaitForSeconds(wave.delay);
+        }
+
+        state = State.WAIT;
+        yield break;
+    }
+
+    private void SpawnEnemy(GameObject _enemy)
+    {
+        if (spawnPoints.Length > 0)
+        {
+            GameObject _spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+            Instantiate(_enemy, _spawnPoint.transform.position, _spawnPoint.transform.rotation);
+        }
+        else
+        {
+            Debug.LogError("Spawn points don't exists!");
+        }
+    }
+}
--- Shooter-Game/Assets/Scripts/Status.cs
+++ Shooter-Game/Assets/Scripts/Status.cs
@@ -7,19 +7,25 @@ public class Status : MonoBehaviour
 {   [SerializeField]
     private RectTransform healthBar;
     [SerializeField]
+    private RectTransform redHealthBar;
+    [SerializeField]
     private Text healthText;
 
     private void Start()
     {
         if (healthText == null) Debug.LogError("No Enemy Text!");
         if (healthBar == null) Debug.LogError("No Health Bar!");
+        if (redHealthBar == null) Debug.LogError("No Red Health Bar!");
     }
 
     public void UpdateHealth(float currentHp, float maxHp)
     {
         float percentHealth = currentHp / maxHp;
+        float redHealth = 1;
+        redHealth -= percentHealth;
 
         healthBar.localScale = new Vector3(percentHealth, healthBar.localScale.y, healthBar.localScale.z);
+        redHealthBar.localScale = new Vector3(redHealth, redHealthBar.localScale.y, redHealthBar.localScale.z);
         healthText.text = currentHp + "/" + maxHp + " HP";
     }
 }
