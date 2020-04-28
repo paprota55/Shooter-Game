8e3fa5a7c84db76917062c30fde588699776f254
paprota55 <44095450+paprota55@users.noreply.github.com>
2020-04-28 15:38:28 +0200

    Added simple menu

---
+++ Shooter-Game/Assets/Scripts/MenuUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuUI : MonoBehaviour
+{
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Debug.Log("Exit game!");
+    }
+}
